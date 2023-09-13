95f87d2a7c68234a84f5b14a7321f19cecb6c793
SID-DIQ <siddiqdec23@gmail.com>
2023-09-13 22:48:40 +0530

    bigest number and possitive number

---
+++ BiggestNumber.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter The First Number: "))
+num2 = int(input("Enter The Number 2: "))
+num3 = int(input("Enter The Number 3: "))
+
+if num1 > num2 and num1 > num3: # Replace CONDITION1 with a valid condition that makes this work
+    print("num1 is bigger than num2 and num3")
+    
+elif num2 > num1 and num2 >num3: # Replace CONDITION3 with a valid condition that makes this work
+    print("num2 is bigger than num1 and num3")
+
+elif num3 > num1 and num3 > num2:
+    print("num3 is bigger then num1 and num2") 
+
+elif num1 == num2 ==num3:
+    print("All The Numbers are equal")
---
+++ PosNEgZero.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter The Number:"))
+
+# Possitive Number
+if num1 > 0:
+    print(str(num1) + " Possitive Number")
+
+# Negative Number
+elif num1 < 0:
+    print(str(num1) + " egative Number")
+
+# Zero
+else:
+    print(str(num1) + " Zero")
