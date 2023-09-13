46006e9f10699e0fe06001f1c67519ffd03ccef8
SID-DIQ <siddiqdec23@gmail.com>
2023-09-13 12:54:10 +0530

    About me

---
+++ Aboutme.py
@@ -0,0 +1,16 @@
+# Create a variable called name and set it equal to your name
+
+
+# Create a variable called age and set it equal to your age
+
+
+# Create a variable called output, and use {} symbols and the format() function to 
+# make it contain a string like the example text in the description
+
+
+# Finally, use the print() function to print the output.
+name ="Siddiq"
+age=20
+string ="My name is {} and I am {} years old."
+output = string.format(name,age)
+print(output)
