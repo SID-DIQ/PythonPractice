b880ee0f8cc4bef551c65882295a92d1c73a5172
SID-DIQ <siddiqdec23@gmail.com>
2023-09-13 12:51:41 +0530

    Strring Joint

---
+++ Broken_string.py
@@ -0,0 +1,9 @@
+# The string in this program is broken.
+
+# Use your knowledge of fixing broken strings to fix it...
+
+# \ will used to join the string
+
+broken_string = 'Hello, I\'m Anthony. What is your name?' 
+
+print(broken_string)
